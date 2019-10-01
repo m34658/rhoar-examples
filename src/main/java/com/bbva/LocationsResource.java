fb73bcfcbb818c24742ecd9e2d2d0e619067192c
m34658 <51213581+m34658@users.noreply.github.com>
2019-10-01 12:30:48 -0500

    Seguimiento al proyecto

---
+++ src/main/java/com/bbva/LocationsResource.java
@@ -0,0 +1,16 @@
+package com.bbva;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/locations")
+public interface LocationsResource {
+  @GET
+  @Produces("application/json")
+  Response generatedMethod1();
+}
